0e1007e110f5eeda190cb035686f5bbe109f7ee3
Magdalena Stenius <magdalena.stenius@bimobject.com>
2018-07-11 14:07:37 +0200

    Functionized.

--- read_from_url.py
+++ read_from_url.py
@@ -1,10 +1,22 @@
 from urllib.request import urlopen
 
-with urlopen('http://sixty-north.com/c/t.txt') as story:
-    story_words = []
-    for line in story:
-        line_words = line.decode('utf-8').split()
-        for word in line_words:
-            story_words.append(word)
+def fetch_words():
+    with urlopen('http://sixty-north.com/c/t.txt') as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for word in line_words:
+                story_words.append(word)
+    return story_words
 
-print(story_words)
+def print_words(story_words):    
+    print(story_words)
+
+def main():
+    words = fetch_words()    
+    print_words(words)
+
+if __name__ == '__main__':
+    main()
+
+# >>> from read_from_url import (print_words, fetch_words) 
