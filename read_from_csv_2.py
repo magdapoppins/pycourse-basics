2e8f253f4b38088c3b3fdbd02874fa42de03cc38
magdapoppins <mmbste@utu.fi>
2018-08-16 21:14:15 +0200

    Added trees csv check. Max_height working.

--- read_from_csv_2.py
+++ read_from_csv_2.py
@@ -1,11 +1,17 @@
 import csv
 
-csv_file = open("../../trees.csv")
+csv_file = open('trees.csv')
 csv_reader = csv.reader(csv_file, delimiter=',')
 
+# Skip first row if it only contains the column headers
+next(csv_reader)
+max_height = 0
+
 for row in csv_reader:
-    index, grith, height, volume = row
-    print(height)
+    Index, Girth, Height, Volume = row
+    if int(Height) > max_height:
+        max_height = int(Height)
 
+print("Heighest tree was {} meters high.".format(max_height))
 
-csv_file.close()    
+csv_file.close()
